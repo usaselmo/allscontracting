beec691c269bf01605340f5d4967074fb263e9ae
Anselmo Ribeiro <6975.usa@gmail.com>
2020-05-18 15:01:11 -0400

    abstrac class small changes

--- src/main/java/com/allscontractingdc/repo/fsimpl/AbstractGenericFSRepository.java
+++ src/main/java/com/allscontractingdc/repo/fsimpl/AbstractGenericFSRepository.java
@@ -4,13 +4,33 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import com.allscontractingdc.exception.LeadsException;
 import com.allscontractingdc.repo.Repository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public abstract class AbstractGenericFSRepository<E, ID> implements Repository<E, ID> {
 
 	abstract Path getPersistenceFile();
+
+	@Override
+	public boolean existsById(ID id) {
+		return this.findById(id).isPresent();
+	}
+
+	@Override
+	public long count() {
+		return Stream.of(this.findAll()).count();
+	}
+
+	@Override
+	public void delete(E entity) {
+		// TODO Auto-generated method stub
+		log.error("delete method not implemented yet");
+	}
 
 	protected void saveToFile(String dataToWriteToFile) throws LeadsException {
 		File leadFile = getPersistenceFile().toFile();
@@ -39,4 +59,5 @@ public abstract class AbstractGenericFSRepository<E, ID> implements Repository<E
 			throw new LeadsException("Could not create File: " + crunchifyFile);
 		}
 	}
+	
 }
