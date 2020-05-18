413f4d8e47cdacef2193af419c9d7d42081f1b41
Anselmo Ribeiro <6975.usa@gmail.com>
2020-05-18 15:02:00 -0400

    create lead repository

--- src/main/java/com/allscontractingdc/repo/fsimpl/LeadRepository.java
+++ src/main/java/com/allscontractingdc/repo/fsimpl/LeadRepository.java
@@ -8,16 +8,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.allscontractingdc.exception.LeadsException;
 import com.allscontractingdc.model.Lead;
-import com.allscontractingdc.tradutor.FSGenericTranslater;
-import com.allscontractingdc.tradutor.FSGenericTranslaterDispatcher;
-import com.allscontractingdc.tradutor.impl.GenericLeadTranslaterImpl;
+import com.allscontractingdc.tradutor.Translater;
+import com.allscontractingdc.tradutor.TranslaterDispatcher;
+import com.allscontractingdc.tradutor.impl.LeadHelper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,19 +24,20 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class LeadRepository extends AbstractGenericFSRepository<Lead, String> { 
 
-	@Autowired	FSGenericTranslaterDispatcher translaterDispatcher;
-	@Autowired  GenericLeadTranslaterImpl leadTranslater;
-
+	@Autowired	TranslaterDispatcher translaterDispatcher;
+	public static final Path PROPOSALS_FOLDER = Paths.get("C:\\Users\\Anselmo.asr\\Google Drive\\All's Contracting\\proposals");
+	
 	@Override
 	public Path getPersistenceFile() {
 		return Paths.get("C:\\Users\\Anselmo.asr\\Google Drive\\All's Contracting\\app\\leads.txt");
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public <S extends Lead> S save(S entity) {
 		if(!this.existsById(entity.getId())) {
 			try {
-				FSGenericTranslater translater = this.translaterDispatcher.dispatch(entity.getVendor());
+				Translater<Lead> translater = (Translater<Lead>) this.translaterDispatcher.dispatch(entity.getVendor());
 				String line = translater.entityToLocalFSFileLine(entity);
 				saveToFile(line);
 			} catch (LeadsException e) {
@@ -51,8 +51,8 @@ public class LeadRepository extends AbstractGenericFSRepository<Lead, String> {
 	public Optional<Lead> findById(String id) {
 		try {
 			return Files.readAllLines(getPersistenceFile()).stream()
-					.filter(line -> leadTranslater.localFileLineToLead(line).getId().equalsIgnoreCase(id))//TODO leadTranslater is wrong
-					.map(line -> leadTranslater.localFileLineToLead(line)).findFirst();//TODO leadTranslater is wrong
+					.filter(line -> LeadHelper.localFSFileLineToEntity(line, Lead.class).getId().equalsIgnoreCase(id))
+					.map(line -> LeadHelper.localFSFileLineToEntity(line, Lead.class)).findFirst();
 		} catch (IOException e) {
 			log.error(e.getMessage());
 			return Optional.empty();
@@ -64,28 +64,12 @@ public class LeadRepository extends AbstractGenericFSRepository<Lead, String> {
 		try {
 			return Files.readAllLines(getPersistenceFile())
 					.stream()
-					.map(line -> leadTranslater.localFileLineToLead(line))//TODO leadTranslater is wrong
+					.map(line -> LeadHelper.localFSFileLineToEntity(line, Lead.class))
 					.sorted( (a,b)-> b.getDate().compareTo(a.getDate()) )
 					.collect(Collectors.toList());
 		} catch (IOException e) {
 			return Collections.emptyList();
 		}
-	}
-
-	@Override
-	public long count() {
-		return Stream.of(this.findAll()).count();
-	}
-
-	@Override
-	public void delete(Lead entity) {
-		// TODO Auto-generated method stub
-		log.error("delete method not implemented yet");
-	}
-
-	@Override
-	public boolean existsById(String id) {
-		return this.findById(id).isPresent();
 	}
 
 }
