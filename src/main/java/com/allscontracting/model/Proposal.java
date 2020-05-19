92c635fad18b468a9ef32abe58cf5a21c1eb4332
Anselmo Ribeiro <6975.usa@gmail.com>
2020-05-19 11:09:08 -0400

    include proposal repository

--- src/main/java/com/allscontracting/model/Proposal.java
+++ src/main/java/com/allscontracting/model/Proposal.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
@@ -17,25 +16,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-//@javax.persistence.Entity(name="name")
+@javax.persistence.Entity(name = "name")
 public class Proposal implements Entity<Long> {
 
 	private static final long serialVersionUID = -8804397870000139075L;
-	
-	@Id @GeneratedValue
+
+	@Id
+	@GeneratedValue
 	private Long id;
-	
-	@OneToMany(mappedBy="proposal_id")
-	private List<Item> items;
-	
-	@OneToMany(mappedBy="proposal_id")
-	private List<Line> lines;
-	
+
+	//@OneToMany(mappedBy = "proposal_id")
+	//private List<Item> items;
+
+	//@OneToMany(mappedBy = "proposal_id")
+	//private List<Line> lines;
+
 	private BigDecimal total;
-	
+
 	private String fileName;
-	
-	
+
 	private Lead lead;
 
 }
